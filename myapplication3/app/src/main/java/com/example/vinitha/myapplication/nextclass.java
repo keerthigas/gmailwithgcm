9527c7e370d3188e218fbc87c527c528aed366aa
vinitha sekar <vinitha sekar>
2016-01-04 16:10:39 +0530

    glogin

---
+++ myapplication3/app/src/main/java/com/example/vinitha/myapplication/nextclass.java
@@ -0,0 +1,20 @@
+package com.example.vinitha.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vinitha on 04-01-2016.
+ */
+public class nextclass extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.content_main);
+
+
+    }
+
+}
